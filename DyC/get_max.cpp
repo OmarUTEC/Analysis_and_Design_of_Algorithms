45ea1283606e250f04f5f3cfd9700710177200f5
OmarUTEC <javier.chavarria@utec.edu.pe>
2023-12-15 20:08:00 -0500

    new recurrence algorithm

---
+++ DyC/get_max.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+
+
+int get_max(int arr[], int start, int end) {
+    // Caso base: si el subconjunto tiene un solo elemento,
+    // ese elemento es el máximo.
+    if (start == end) {
+        return arr[start];
+    }
+
+    // Calcula el índice del medio del subconjunto actual.
+    // Este paso divide el problema en dos partes.
+    int middle = floor((start + end) / 2);
+
+    // Llamada recursiva para encontrar el máximo en la parte izquierda del array,
+    // desde el índice 'start' hasta 'middle'.
+    int left_max = get_max(arr, start, middle);
+
+    // Llamada recursiva para encontrar el máximo en la parte derecha del array,
+    // desde el índice 'middle + 1' hasta 'end'.
+    int right_max = get_max(arr, middle + 1, end);
+
+    // Retorna el mayor valor entre los máximos de las dos mitades.
+    // Este paso combina las soluciones de las partes izquierda y derecha.
+    return (left_max > right_max) ? left_max : right_max;
+}
+
+
+int main(){
+
+    const int arr_size = 10;
+    int arr[arr_size];
+
+    // Inicializando el generador de números aleatorios
+    srand(time(0));
+
+    // Llenando el array con números aleatorios entre 100 y 1000
+    for(int i = 0; i < arr_size; i++){
+        arr[i] = rand() % 901 + 100; // Números entre 100 y 1000
+    }
+
+    std::cout << "Array: ";
+    for(int i = 0; i < arr_size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    int max_value = get_max(arr, 0, arr_size -1);
+    std::cout << max_value << std::endl;
+
+    return 0;
+}
--- DyC/merge_sort.cpp
+++ DyC/merge_sort.cpp
@@ -1,53 +1,3 @@
-
-/*
-#include <iostream>
-
-void merge(int Arr[], int p, int q, int r){
-    int n1 = q-p+1;
-    int n2 = r-q;
-    int L[n1+1], R[n2+1];
-
-    for(int i=1; i<n1; i++){
-        L[i] = Arr[p+i-1];
-    }
-
-    for(int j=1; j<n2; j++){
-        R[j] = Arr[q+j];
-    }
-
-    L[n1+1] = 1000000;
-    R[n2+1] = 1000000;
-
-    int i=1, j=1;
-
-    for(int k=p; k<r; k++){
-        if(L[i] <= R[j]){
-            Arr[k] = L[i];
-            i++;
-        }
-        else{
-            Arr[k] = R[j];
-            j++;
-        }
-    }
-}
-
-void merge_sort(int Arr[], int p, int r){
-    if(p<r){
-        int q = (p+r)/2;
-        merge_sort(Arr, p, q);
-        merge_sort(Arr, q+1, r);
-        merge(Arr, p, q, r);
-    }
-}
-
-int main(){
-
-
-    return 0;
-}
-*/
-
 #include <iostream>
 #include <climits>
 #include <cmath>
@@ -94,14 +44,23 @@ void merge_sort(int Arr[], int p, int r){
 }
 
 int main(){
-    int arr[] = {5, 2, 4, 4, 7, 1, 3, -2, 6};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    const int arr_size = 10;
+    int arr[arr_size];
+
+    // Inicializando el generador de números aleatorios
+    srand(time(0));
+
+    // Llenando el array con números aleatorios entre 100 y 1000
+    for(int i = 0; i < arr_size; i++){
+        arr[i] = rand() % 901 + 100; // Números entre 100 y 1000
+    }
 
     cout << "Array before sorting: ";
     for(int i = 0; i < arr_size; i++)
         cout << arr[i] << " ";
     cout << endl;
 
+    // Llamada a tu función merge_sort
     merge_sort(arr, 0, arr_size - 1);
 
     cout << "Array after sorting: ";
