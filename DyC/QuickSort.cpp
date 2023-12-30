a1080ef68473c0d1081a86fe5b380141b4b7a719
OmarUTEC <javier.chavarria@utec.edu.pe>
2023-12-30 14:49:20 -0500

    update algorithm quicksort

--- DyC/QuickSort.cpp
+++ DyC/QuickSort.cpp
@@ -1,52 +1,56 @@
 #include <iostream>
-#include <vector>
 
-// Función de partición para QuickSort
-int partition(std::vector<int> &arr, int p, int r) {
+using namespace std;
+
+// Función para intercambiar dos elementos en un array
+void exchange(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+// Función para dividir el array y realizar el particionamiento
+int partition(int arr[], int p, int r) {
     int x = arr[r];
     int i = p - 1;
-
-    for (int j = p; j < r; ++j) {
+    
+    for (int j = p; j <= r - 1; j++) {
         if (arr[j] <= x) {
             i++;
-            std::swap(arr[i], arr[j]);
+            exchange(arr[i], arr[j]);
         }
     }
-
-    std::swap(arr[i + 1], arr[r]);
+    exchange(arr[i + 1], arr[r]);
     return i + 1;
 }
 
-// Función QuickSort
-void quickSort(std::vector<int> &arr, int p, int r) {
+// Función Quicksort principal
+void quicksort(int arr[], int p, int r) {
     if (p < r) {
         int q = partition(arr, p, r);
-        quickSort(arr, p, q - 1);
-        quickSort(arr, q + 1, r);
+        quicksort(arr, p, q - 1);
+        quicksort(arr, q + 1, r);
     }
-}
-
-// Función de prueba
-void testQuickSort() {
-    std::vector<int> arr = {12, 9, 7, 15, 10, -23, 4, 6};
-    int n = arr.size();
-
-    std::cout << "Array antes de ordenar: ";
-    for (int i = 0; i < n; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-
-    quickSort(arr, 0, n - 1);
-
-    std::cout << "Array ordenado: ";
-    for (int i = 0; i < n; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
 }
 
 int main() {
-    testQuickSort();
+    // Ejemplo de uso
+    int array[] = {12, 9, 2, 2, 3, 7, 14, -11};
+    int n = sizeof(array) / sizeof(array[0]);
+
+    cout << "Array original: ";
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    quicksort(array, 0, n - 1);
+
+    cout << "Array ordenado: ";
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
