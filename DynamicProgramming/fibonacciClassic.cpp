be46e80aac257f516a8c5465513d2278e1078254
OmarUTEC <javier.chavarria@utec.edu.pe>
2024-01-14 20:02:47 -0500

    add code line

--- DynamicProgramming/fibonacciClassic.cpp
+++ DynamicProgramming/fibonacciClassic.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
-
 using namespace std;
-
 // Recursive function to print the Fibonacci numbers
-int fibonacci(int n){
 
+int fibonacci(int n){
     if(n == 0 || n == 1){
         return n;
     }
-
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
+// main function
 int main(){
-
     int n = 12;
-
     cout << fibonacci(n) << endl;
-
     return 0;
 }
